7e1b3e01b5b81ce9c03860056fd4b361002ecf91
KMGeon <103854287+KMGeon@users.noreply.github.com>
2022-06-06 20:35:44 +0900

    This is git practice

---
+++ src/charon1/charon1.java
@@ -0,0 +1,20 @@
+package charon1;
+
+public class charon1 {
+
+	public static void main(String[] args) {
+	
+		
+		int i,j,k;
+		for(i=0;i<5;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.print('*');
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module charon1 {
+}
